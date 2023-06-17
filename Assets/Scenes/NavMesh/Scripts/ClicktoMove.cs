181a84d4d6540faf980c16eaf2cd10bbd0ff68e4
Computer-Worm <yusufhasarr@gmail.com>
2023-06-17 16:21:24 +0300

    Added new models

--- Assets/Scenes/NavMesh/Scripts/ClicktoMove.cs
+++ Assets/Scenes/NavMesh/Scripts/ClicktoMove.cs
@@ -10,22 +10,21 @@ public class ClicktoMove : MonoBehaviour
     NavMeshAgent m_Agent;
     RaycastHit m_HitInfo = new RaycastHit();
 
-
     private void Start()
     {
-        m_Agent = GetComponent<NavMeshAgent>();
+        m_Agent= GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
             if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
             {
                 m_Agent.destination = m_HitInfo.point;
             }
         }
     }
+
 }
