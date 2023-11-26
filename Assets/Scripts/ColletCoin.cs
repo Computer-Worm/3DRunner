7f42d2388adcb1036a0708011268a966ceb23786
Computer-Worm <yusufhasarr@gmail.com>
2023-11-26 19:27:25 +0300

    Added Win-Lose Animations and Added Restart Panel
    Added Win-Lose Animations and Added Restart Panel

--- Assets/Scripts/ColletCoin.cs
+++ Assets/Scripts/ColletCoin.cs
@@ -10,6 +10,15 @@ public class ColletCoin : MonoBehaviour
     public int scrore;
     public TextMeshProUGUI CoinText;
     public PlayerController playerController;
+    public int maxScore;
+    public Animator PlayerAnim;
+    public GameObject Player;
+    public GameObject EndPanel;
+
+    private void Start()
+    {
+        PlayerAnim = Player.GetComponentInChildren<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,9 +30,27 @@ public class ColletCoin : MonoBehaviour
 
         else if(other.CompareTag("End"))
         {
-            Debug.Log("Baþardýn!..");
             playerController.runningSpeed = 0;
+            transform.Rotate(transform.rotation.x, 180, transform.rotation.y, Space.Self);
+            EndPanel.SetActive(true);
+
+            if(scrore >= maxScore)
+            {
+                //Debug.Log("You Win!");
+                PlayerAnim.SetBool("win", true);
+            }
+
+            else
+            {
+                //Debug.Log("You Lose!");
+                PlayerAnim.SetBool("lose", true);
+            }
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnCollisionEnter(Collision collision)
